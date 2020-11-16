0ad941d08d172903efa3d0215f9c88fe2349ada2
Navneet <navneetjh5@gmail.com>
2020-11-16 19:50:06 +0530

    INITITATED:HOTEL RESERVATION Program

---
+++ src/main/java/com/cg/hotelreservation/HotelReservation.java
@@ -0,0 +1,7 @@
+package com.cg.hotelreservation;
+
+public class HotelReservation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to HOTEL RESERVATION program");
+    }
+}
